fdfc89e76df851c4d01bf63e511c6479483865ae
Hari Balamurali <haribala246@gmail.com>
2023-06-11 16:58:00 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,7 +1,9 @@
+import java.io.File;
 public class Driver 
 {
-	public static void main(String [] args) 
+	public static void main(String [] args) throws Exception
 	{
+		/* 
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
 		double [] c1 = {6,0,0,5};
@@ -14,5 +16,45 @@ public class Driver
 			System.out.println("1 is a root of s");
 		else
 			System.out.println("1 is not a root of s");
+		*/
+		double[] a1 = {6, -2, 5, 8};
+		int [] b1 = {0, 1, 4, 8};
+		Polynomial p1 = new Polynomial(a1, b1);
+		double[] a2 = {3, 4, -7, -5, 9};
+		int [] b2 = {2, 2, 3, 6, 3};
+		Polynomial p2 = new Polynomial(a2, b2);
+		Polynomial p3 = p1.add(p2);
+		printPoly(p3);
+		System.out.println(p2.evaluate(3));
+		if (p2.hasRoot(0));
+		{
+			System.out.println("0 is a root of p2");
+		}
+		double[] a3 = {3, 2, 5};
+		int[] b3 = {0, 2, 1};
+		double[] a4 = {2, 4, 1};
+		int[] b4 = {2, 3, 4};
+		Polynomial p4 = new Polynomial(a3, b3);
+		Polynomial p5 = new Polynomial(a4, b4);
+		Polynomial p6 = p4.multiply(p5);
+		printPoly(p6);
+		File f = new File("newtext.txt");
+		Polynomial p7 = new Polynomial(f);
+		printPoly(p7);
+		p4.saveToFile("newtext.txt");
+	}
+
+	public static void printPoly (Polynomial p)
+	{
+		System.out.println("Printing Coefficients:");
+		for (int i = 0; i < p.coefs.length; i++)
+		{
+			System.out.println(p.coefs[i]);
+		}
+		System.out.println("Printing Exponents:");
+		for (int i = 0; i < p.exps.length; i++)
+		{
+			System.out.println(p.exps[i]);
+		}
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,40 +1,118 @@
-
-
+import java.util.Scanner;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.Math;
 public class Polynomial
 {
 	double [] coefs;
+	int [] exps;
 	
 	public Polynomial()
 	{
-		this.coefs = new double[0];
+		this.coefs = null;
+		this.exps = null;
 	}
 	
-	public Polynomial (double [] newcoefs)
+	public Polynomial (double [] newcoefs, int [] newexps)
 	{
 		this.coefs = newcoefs;
+		this.exps = newexps;
 	}
 	
-	public Polynomial add (Polynomial other)
+	public Polynomial (File f) throws Exception
 	{
-		int length = 0;
-		if (other.coefs.length > coefs.length)
+		Scanner scan = new Scanner(f);
+		if (!scan.hasNextLine())
 		{
-			length = other.coefs.length;
+			this.coefs = null;
+			this.exps = null;
 		}
 		else
 		{
-			length = coefs.length;
+			String poly = scan.nextLine();
+			poly = poly.replace("-","+-" );
+			String[] terms = poly.split("\\+");
+			this.coefs = new double[terms.length];
+			this.exps = new int[terms.length];
+			for (int i = 0; i < terms.length; i++)
+			{
+				String[] term = terms[i].split("x");
+				this.coefs[i] = Double.parseDouble(term[0]);
+				if (term.length <= 1)
+				{
+					this.exps[i] = 0;
+				}
+				else
+				{
+					this.exps[i] = Integer.parseInt(term[1]);
+				}
+			}
 		}
-		double[] poly = new double[length];
-		for (int i  = 0; i < coefs.length; i++)
+		scan.close();
+	}
+
+	private int[] fillup (int[] fill)
+	{
+		for (int i = 0; i < fill.length; i++)
 		{
-			poly[i] = coefs[i];
+			fill[i] = i;
 		}
-		for (int i = 0; i < length; i++)
+		return fill;
+	}
+
+
+	
+	private int max(int[] dub)
+	{
+		int m = dub[0];
+		for (int i = 0; i < dub.length; i++)
 		{
-			poly[i] += other.coefs[i];
+			if (dub[i] > m)
+			{
+				m = dub[i];
+			}
 		}
-		Polynomial p = new Polynomial(poly);
+		return m;
+	}
+
+	public Polynomial add (Polynomial other)
+	{
+		int m1 = max(this.exps);
+		int m2 = max(other.exps);
+		int[] newexps = new int[Math.max(m1, m2) + 1];
+		double [] newcoefs = new double[Math.max(m1, m2) + 1];
+		newexps = fillup(newexps);
+		for (int i = 0; i < this.coefs.length; i++)
+		{
+			newcoefs[this.exps[i]] = this.coefs[i];
+		}
+		for (int i = 0; i < other.coefs.length; i++)
+		{
+			newcoefs[other.exps[i]] += other.coefs[i];
+		}
+		int count = 0;
+		for (int i = 0; i < newcoefs.length; i++)
+		{
+			if (newcoefs[i] != 0)
+			{
+				count++;
+			}
+		}
+		int[] nexps = new int[count];
+		double[] ncoefs = new double[count];
+		count = 0;
+		for (int i = 0; i < newexps.length; i++)
+		{
+			if (newcoefs[i] != 0)
+			{
+				ncoefs[count] = newcoefs[i];
+				nexps[count] = i;
+				count++;
+			}
+		}
+		Polynomial p = new Polynomial(ncoefs, nexps);
 		return p;
 	}
 	
@@ -43,7 +121,7 @@ public class Polynomial
 		double sum = 0;
 		for (int i = 0; i < coefs.length; i++)
 		{
-			sum += coefs[i] * Math.pow(num, i );
+			sum += coefs[i] * Math.pow(num, exps[i]);
 		}
 		return sum;
 	}
@@ -53,4 +131,78 @@ public class Polynomial
 		double sum = evaluate(num);
 		return sum == 0;
 	}
+	
+
+	private int maxSum(int[] arr)
+	{
+		int sum = 0;
+		for (int i = 0; i < this.exps.length; i++)
+		{
+			for (int j = 0; j < arr.length; j++)
+			{
+				if (this.exps[i] + arr[j] > sum)
+				{
+					sum = this.exps[i] + arr[j];
+				}
+			}		
+		}
+		return sum;
+	}
+
+	public Polynomial multiply(Polynomial other)
+	{
+		int max = maxSum(other.exps);
+		int[] tempexps = new int[max + 1];
+		double [] tempcoefs = new double[max + 1];
+		tempexps = fillup(tempexps);
+		for (int i = 0; i < this.exps.length; i++)
+		{
+			for (int j = 0; j < other.exps.length; j++)
+			{
+				tempcoefs[this.exps[i] + other.exps[j]] += this.coefs[i] * other.coefs[j];
+			}
+		}
+		int count = 0; 
+		for (int i = 0; i < tempcoefs.length; i++)
+		{
+			if (tempcoefs[i] != 0)
+			{
+				count++;
+			}
+		}
+		double[] newcoefs = new double[count];
+		int[] newexps = new int[count];
+		count = 0;
+		for (int i = 0; i < tempcoefs.length; i++)
+		{
+			if (tempcoefs[i] != 0)
+			{
+				newcoefs[count] = tempcoefs[i];
+				newexps[count] = tempexps[i];
+				count++; 
+			}
+		}
+		Polynomial p = new Polynomial(newcoefs, newexps);
+		return p;
+	}
+	public void saveToFile (String filename) throws Exception
+	{
+		File f = new File(filename);
+		String save = "";
+		for (int i = 0; i < this.coefs.length; i++)
+		{
+			save += this.coefs[i];
+			if (this.exps[i] != 0)
+			{
+				save += "x" + this.exps[i];
+			}
+			save += "+";
+		}
+		save = save.replace("+-", "-");
+		save = save.substring(0, save.length() - 1);
+		FileWriter fw = new FileWriter(f);
+		fw.write(save);
+		fw.close();
+	}
 }
+
